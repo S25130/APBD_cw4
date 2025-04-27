5a814bacf4a2846558c9c08a26ef46766afe6b1a
S25130 <s25130@pjwstk.edu.pl>
2025-04-27 19:46:44 +0200

    Update DataStore.cs

--- S25130/APBD_cw4/Models/DataStore.cs
+++ S25130/APBD_cw4/Models/DataStore.cs
@@ -1,6 +1,19 @@
+using APBD_cw4.Models;
+
 namespace APBD_cw4.Models;
 
 public class DataStore
 {
-    
+    public static List<Animal> Animals { get; set; } = new()
+    {
+        new Animal { Id = 1, Name = "Reksio", Category = "Pies", Weight = 10.5f, FurColor = "Brązowy" },
+        new Animal { Id = 2, Name = "Mruczek", Category = "Kot", Weight = 4.2f, FurColor = "Czarny" }
+    };
+
+    public static List<Visit> Visits { get; set; } = new()
+    {
+        new Visit { Id = 1, AnimalId = 1, Date = DateTime.Now.AddDays(-10), Description = "Szczepienie", Price = 150 },
+        new Visit { Id = 2, AnimalId = 2, Date = DateTime.Now.AddDays(-5), Description = "Odrobaczanie", Price = 100 }
+    };
+
 }
