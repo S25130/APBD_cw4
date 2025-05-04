65f0087a956f4c4565cfbfe7e558a90e6943c83e
S25130 <s25130@pjwstk.edu.pl>
2025-05-04 17:17:22 +0200

    Update DataStore.cs

--- S25130/APBD_cw4/Models/DataStore.cs
+++ S25130/APBD_cw4/Models/DataStore.cs
@@ -6,14 +6,16 @@ public class DataStore
 {
     public static List<Animal> Animals { get; set; } = new()
     {
-        new Animal { Id = 1, Name = "Reksio", Category = "Pies", Weight = 10.5f, FurColor = "Brązowy" },
-        new Animal { Id = 2, Name = "Mruczek", Category = "Kot", Weight = 4.2f, FurColor = "Czarny" }
+        new Animal { Id = 1, Name = "Reksio", Category = "Pies", Weight = 10.3f, FurColor = "Brązowo-biały" },
+        new Animal { Id = 2, Name = "Puszek", Category = "Kot", Weight = 4.6f, FurColor = "Biały" }
     };
 
     public static List<Visit> Visits { get; set; } = new()
     {
-        new Visit { Id = 1, AnimalId = 1, Date = DateTime.Now.AddDays(-10), Description = "Szczepienie", Price = 150 },
-        new Visit { Id = 2, AnimalId = 2, Date = DateTime.Now.AddDays(-5), Description = "Odrobaczanie", Price = 100 }
+        new Visit { Id = 1, AnimalId = 1, Date = DateTime.Now.AddDays(-10), Description = "Szczepienie", Price = 195 },
+        new Visit { Id = 2, AnimalId = 2, Date = DateTime.Now.AddDays(-5), Description = "Odrobaczanie", Price = 100 },
+        new Visit { Id = 3, AnimalId = 2, Date = DateTime.Now.AddDays(-15), Description = "Kastracja", Price = 555 },
+        new Visit { Id = 4, AnimalId = 1, Date = DateTime.Now.AddDays(-29), Description = "Usuwanie kamienia nazębnego", Price = 1475 }
     };
 
 }
